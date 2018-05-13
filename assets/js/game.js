4cf52a303f73a1d26695d63aa4a6831353128025
R <merinogeospatial@gmail.com>
2018-05-13 16:07:56 -0400

    reults timer created

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,24 +1,35 @@
 let count = 10;
+var resultCount = 5;
 
-function decrement() {
+function decrementGame() {
     count--;
     $('#countdown').text(count);
     if (count === 0) {
         clearInterval(timer)
+        // call results here
+    }
+}
+
+function startClock() {
+    if (count != 0){
+    timer = setInterval(decrementGame,1000);
+    }
+}
+
+function decrementResult() {
+    resultCount--;
+    $('#question').text(resultCount); // change div or no display
+    if (resultCount === 0) {
+        clearInterval(resultTimer)
+        // start next question here
+    }
+}
+
+function resultClock() {
+    if (resultCount != 0){
+    resultTimer = setInterval(decrementResult,1000);
     }
 }
 
 
-function startClock() {
-        if (count != 0){
-        timer = setInterval(decrement,1000);
-        console.log(count);
-        }
-    
-        else {
-        clearInterval(timer);
-        console.log('time up')
-        }
-    }
 
-startClock();
