0126e6e5ac7962bfa61e3f6b7ca85a24d6f4cdef
R <merinogeospatial@gmail.com>
2018-05-14 06:54:42 -0400

    start building trivia object model to hold questions and answers

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,35 @@
+trivia = {
+    question: [
+        "There's too many people on this Earth. We need a new plague.",
+        "I really should have a Tweeter account."
+
+    ],
+    a1: [
+        "Angela",
+        "Andy"
+
+    ],
+    a2: [
+        "Dwight",
+        "Michael"
+
+    ],
+    a3: [
+        "Creed",
+        "Kevin"
+
+    ],
+    a4: [
+        "Michael",
+        "Dwight"
+    ]
+}
+
+
 let count = 10;
 var resultCount = 5;
 
+// Brings the count down from 10 to 0
 function decrementGame() {
     count--;
     $('#countdown').text(count);
@@ -10,21 +39,24 @@ function decrementGame() {
     }
 }
 
+// Initializes countdown for questions
 function startClock() {
     if (count != 0){
     timer = setInterval(decrementGame,1000);
     }
 }
 
+// Brings the count down from 5 to 0
 function decrementResult() {
     resultCount--;
-    $('#question').text(resultCount); // change div or no display
+    $('#question').text(resultCount); // make a countdown div to next question
     if (resultCount === 0) {
         clearInterval(resultTimer)
         // start next question here
     }
 }
 
+// Initializes countdown on results page to next question
 function resultClock() {
     if (resultCount != 0){
     resultTimer = setInterval(decrementResult,1000);
@@ -32,4 +64,8 @@ function resultClock() {
 }
 
 
-
+$('#question').text(trivia.question[0]);
+$('#answer-1').text(trivia.a1[0]);
+$('#answer-2').text(trivia.a2[0]);
+$('#answer-3').text(trivia.a3[0]);
+$('#answer-4').text(trivia.a4[0]);
